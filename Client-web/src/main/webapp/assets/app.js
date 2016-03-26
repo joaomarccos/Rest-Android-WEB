5016c009e7552439544f2029908a9ac23e376c8a
joaomarccos <joaomarccos.ads@gmail.com>
2016-03-26 11:35:33 -0300

    reset text variabe in function filter

--- Client-web/src/main/webapp/assets/app.js
+++ Client-web/src/main/webapp/assets/app.js
@@ -2,18 +2,18 @@ function filter(input) {
     var cont = 0;
     var key = $(input).val();
     var text = "";
-    $(".media").each(function () {
+    $(".media").each(function () {        
         cont++;
         $(this).show();
         text += $(this).find("h4").html();
         $(this).find("span").each(function () {
             text += $(this).html();
-        });
-
+        });        
         if (key !== "" && text.toLowerCase().indexOf(key.toLowerCase()) === -1) {
             $(this).hide();
             cont--;
         }
+        text="";
     });
     
     $("#nresults").empty().html("Exibindo " + cont + " resultados(s)");
